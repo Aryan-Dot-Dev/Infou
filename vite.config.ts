77d579e0c6e93d3002b92ed92129b97c8b5743bc
Aryan-Dot-Dev <aryan.main@proton.me>
2026-01-18 15:25:09 +0530

    changed build

--- vite.config.ts
+++ vite.config.ts
@@ -12,13 +12,5 @@ export default defineConfig({
     }
   },
   base: '/scan/',
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name].js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name].[ext]'
-      }
-    }
-  }
+  
 })
